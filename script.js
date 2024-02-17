882ae52140aea819e923c62479c6f7e900e8b064
Samu-12 <samikshasubhashgedam>
2024-02-17 13:45:01 +0530

    Responsive Landing page

---
+++ script.js
@@ -0,0 +1,38 @@
+const initSlider = () => {
+    const imageList = document.querySelector(".slider .slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider .slider-wrapper .slide-button");
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({left: scrollAmount, behavior: "smooth"});
+        });
+    });
+}
+
+window.addEventListener("load", initSlider);
+
+
+//javascript for incrementing and decrementing the number of items
+
+const plus = document.querySelector(".plus"),
+ minus = document.querySelector(".minus"),
+ num = document.querySelector(".num");
+
+let a = 1;
+
+plus.addEventListener("click", ()=>{
+    a++;
+    a = ( a < 10 ) ? "0" + a : a;
+    num.innerHTML = a;
+    console.log(a);
+})
+
+minus.addEventListener("click", ()=>{
+    if(a<10){
+        a--;
+        a = (a < 10) ? "0" + a : a;
+        num.innerHTML = a;
+    }
+})
